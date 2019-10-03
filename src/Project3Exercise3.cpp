47d704ab955a360520ebecfd7f0c5c842e636db0
Aidan Curtis <>
2019-10-02 19:55:01 -0500

    twisty cool benchmark

--- src/Project3Exercise3.cpp
+++ src/Project3Exercise3.cpp
@@ -96,7 +96,68 @@ void benchmarkCubicles()
 
 void benchmarkTwistycool()
 {
-	// TODO
+	app::SE3RigidBodyPlanning setup;
+	std::string benchmark_name;
+	double runtime_limit, memory_limit;
+	int run_count;
+
+    benchmark_name = std::string("Twistycool");
+    std::string robot_fname = std::string(OMPLAPP_RESOURCE_DIR) + "/3D/Twistycool_robot.dae";
+    std::string env_fname = std::string(OMPLAPP_RESOURCE_DIR) + "/3D/Twistycool_env.dae";
+    setup.setRobotMesh(robot_fname);
+    setup.setEnvironmentMesh(env_fname);
+
+    base::ScopedState<base::SE3StateSpace> start(setup.getSpaceInformation());
+    start->setX(270.);
+    start->setY(160.);
+    start->setZ(-200.);
+    start->rotation().setIdentity();
+
+    base::ScopedState<base::SE3StateSpace> goal(start);
+    goal->setX(270.);
+    goal->setY(160.);
+    goal->setZ(-400.);
+    goal->rotation().setIdentity();
+
+    base::RealVectorBounds bounds(3);
+    bounds.setHigh(0, 350.);
+    bounds.setHigh(1, 250.);
+    bounds.setHigh(2, -150.);
+    bounds.setLow(0, 200.);
+    bounds.setLow(1, 75.);
+    bounds.setLow(2, -450.);
+    setup.getStateSpace()->as<base::SE3StateSpace>()->setBounds(bounds);
+
+    setup.setStartAndGoalStates(start, goal);
+    setup.getSpaceInformation()->setStateValidityCheckingResolution(0.01);
+
+    runtime_limit = 60.0;
+    memory_limit = 10000.0;  // set high because memory usage is not always estimated correctly
+    run_count = 50;
+
+
+	tools::Benchmark::Request request(runtime_limit, memory_limit, run_count);
+	tools::Benchmark b(setup, benchmark_name);
+
+	// optionally set pre & pos run events
+	b.setPreRunEvent([](const base::PlannerPtr &planner) { preRunEvent(planner); });
+	b.setPostRunEvent(
+		[](const base::PlannerPtr &planner, tools::Benchmark::RunProperties &run) { postRunEvent(planner, run); });
+
+	b.addPlanner(std::make_shared<geometric::RRT>(setup.getSpaceInformation()));
+	b.addPlanner(std::make_shared<geometric::EST>(setup.getSpaceInformation()));
+	b.addPlanner(std::make_shared<geometric::PRM>(setup.getSpaceInformation()));
+	b.addPlanner(std::make_shared<geometric::RTP>(setup.getSpaceInformation()));
+
+	// // run all planners with a uniform valid state sampler on the benchmark problem
+	// setup.getSpaceInformation()->setValidStateSamplerAllocator(
+	//     [](const base::SpaceInformation *si) -> base::ValidStateSamplerPtr {
+	//         return std::make_shared<base::UniformValidStateSampler>(si);
+	//     });
+	// b.addExperimentParameter("sampler_id", "INTEGER", "0");
+	b.benchmark(request);
+	b.saveResultsToFile();
+	
 }
 
 int main(int /* argc */, char ** /* argv */)
